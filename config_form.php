3aa27d733a43e744d22c9dbae085a6996ce9afde
aaronknoll <Aaron.Knoll@gmail.com>
2012-03-29 17:30:42 -0400

    Okay, you can change via admin. The CSS as PHP thing throwing errors right now.

--- config_form.php
+++ config_form.php
@@ -1,13 +1,45 @@
 <?php
 $db = get_db();
+$mysql = 'SELECT * FROM '. $db->prefix .'elements
+	WHERE 
+		element_set_id = "1" 
+		ORDER BY '. $db->prefix .'elements.order ASC';
+$our_crazy_array = $db->fetchAll($mysql);	
+$count_crazy_array = count($our_crazy_array);
+echo "$count_crazy_array has  djhdfgjk items";
 
+for($x=0;$x<$count_crazy_array;$x++)
+	{
+	//option looks like "titleon" / "creatoron"
+	$what_is_this_options_status	=	get_option('simplifyon'.$our_crazy_array[$x][id].'');
+	?>
+	<div class="field">
+	<label for="per_page">Turn on <?php echo $our_crazy_array[$x][name]; ?></label>
+	<div class="inputs">
+		<select id="simplifyon<?php echo $our_crazy_array[$x][id]; ?>" name="simplifyon<?php echo $our_crazy_array[$x][id]; ?>">
+	<?php
+	//can only have two options, "yes" oe "no", all lowercase.
+	if($what_is_this_options_status == "no")
+		{ 
+			?>
+			<option value="yes">Show Me</option>
+			<option SELECTED value="no">Hidden</option>
+			<?php
+		}
+	else
+		{
+			?>
+			<option SELECTED value="yes">Show Me</option>
+			<option value="no">Hidden</option>
+			<?php	
+		}
+		?>
+		</select> 
+	<p class="explanation"><?php echo $our_crazy_array[$x][description]; ?></p>
+	</div>
+</div>
+	<?php	
+	}
 ?>
 
 
-<div class="field">
-	<label for="per_page">Number of Questions per Item</label>
-	<div class="inputs">
-	<input type="text" class="textinput"  name="per_page" size="4" value="<?php echo get_option('focusq_per_page'); ?>" id="per_page" />
-	<p class="explanation">The number of questions available per item. No max, recommended 5 or less. </p>
-	</div>
-</div>
--- plugin.php
+++ plugin.php
@@ -4,17 +4,62 @@ add_plugin_hook('public_theme_header', 'simplify_public_theme_header');
 add_plugin_hook('admin_theme_header', 'simplify_admin_theme_header');
 add_plugin_hook('config_form', 'simplify_config_form');
 add_plugin_hook('config', 'simplify_config');
+add_plugin_hook('install', 'simplify_install');
+add_plugin_hook('uninstall', 'simplify_uninstall');
+
+//suggestions
+//consolidate the "get the list"into its own list of functions
+// figure out the array of dublin core options
+// and pass that array around the script, rather than going to the DB too
+// many times. 
+
+function simplify_uninstall()
+{
+	$db = get_db();
+	$mysql = 'SELECT * FROM '. $db->prefix .'elements
+			WHERE 
+			element_set_id = "1" 
+			ORDER BY '. $db->prefix .'elements.order ASC';
+	$our_crazy_array = $db->fetchAll($mysql);	
+	$count_crazy_array = count($our_crazy_array);
+
+	for($x=0;$x<$count_crazy_array;$x++)
+	{
+		//there should be 15 of these. Unless someone hacked and modified the code...
+	 	delete_option('simplifyon'.$our_crazy_array[$x][id].'');
+	}
+}
+
+function simplify_install()
+{
+	$db = get_db();
+	$mysql = 'SELECT * FROM '. $db->prefix .'elements
+			WHERE 
+			element_set_id = "1" 
+			ORDER BY '. $db->prefix .'elements.order ASC';
+	$our_crazy_array = $db->fetchAll($mysql);	
+	$count_crazy_array = count($our_crazy_array);
+
+	for($x=0;$x<$count_crazy_array;$x++)
+	{
+		//there should be 15 of these. Unless someone hacked and modified the code...
+	 	set_option('simplifyon'.$our_crazy_array[$x][id].'', 'yes');
+	 }
+ }
 
 function simplify_admin_theme_header($request)
 {
-    if ($request->getController() == 'items' && $request->getAction() == 'edit') {
-        queue_js('my-script');
-    }
+   //find the absolute path!
+   //include 'simplify.php';
+var_dump($_SERVER);
+   ?>
+   <link rel="stylesheet" type="text/css" href="" />
+	<?php
 }
 
 function simplify_public_theme_header($request)
 {
-    queue_css('my-stylesheet');	
+   // queue_css('my-stylesheet');	
 }
 
 function simplify_config_form()
@@ -23,9 +68,20 @@ include 'config_form.php';
 }
 
 function simplify_config()
-{   
+{
+	$db = get_db();
+	$mysql = 'SELECT * FROM '. $db->prefix .'elements
+	WHERE 
+		element_set_id = "1" 
+		ORDER BY '. $db->prefix .'elements.order ASC';
+	$our_crazy_array = $db->fetchAll($mysql);	
+	$count_crazy_array = count($our_crazy_array);
+
+for($x=0;$x<$count_crazy_array;$x++)
+	{   
     // Use the form to set a bunch of default options in the db
-    $perPage = (int)$_POST['per_page'];
-    set_option('focusq_per_page', $perPage);
+    $onoffswitch = $_POST['simplifyon'.$our_crazy_array[$x][id].''];
+    set_option('simplifyon'.$our_crazy_array[$x][id].'', $onoffswitch);
+	}
 }
     
